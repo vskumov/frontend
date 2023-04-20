5e4c205943eb8b35842b67ea1bb75cf257e83783
vskumov <v.kumov@gmail.com>
2023-04-20 05:19:41 +0300

    auto, it guys, dogs, films

--- FE/hw/script-hw3.js
+++ FE/hw/script-hw3.js
@@ -173,32 +173,26 @@ const dogs = [
         breed: "golden retriever",
         age: 7,
     },
-
     {
         breed: "labrador retriever",
         age: 4,
     },
-
     {
         breed: "french bulldog",
         age: 12,
     },
-
     {
         breed: "beagle",
         age: 6,
     },
-
     {
         breed: "german shepherd dog",
         age: 2,
     },
-
     {
         breed: "poodle",
         age: 3,
     },
-    
     {
         breed: "bulldog",
         age: 4,
